f770413f4381a1e73b2a6efdffd40e66320928a5
uzoolove <uzoolove@gmail.com>
2021-01-20 09:20:22 +0900

    .

--- server/net/tcpclient.js
+++ server/net/tcpclient.js
@@ -1,5 +1,18 @@
 const net = require('net');
 var target = {
   host: 'localhost',
-  port: 2345
+  port: 1234
 };
+
+// 1. 소켓 생성
+var socket = new net.Socket();
+// 2. 서버 접속
+socket.connect(target.port, target.host, function(){
+  console.log(`서버 접속. ${target.host}:${target.port}`);
+  // 3. 서버와 메세지 송수신
+  socket.write('hello');
+  socket.on('data', function(data){
+    console.log('서버로부터 수신한 메세지: ' + data);
+  });
+});
+
