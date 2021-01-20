c1596146589e28aea34a022b6282cae920c80476
uzoolove <uzoolove@gmail.com>
2021-01-20 10:29:27 +0900

    .

--- server/net/tcpclient.js
+++ server/net/tcpclient.js
@@ -2,8 +2,10 @@ const net = require('net');
 var target = {
   // host: 'localhost',
   // port: 1234
-  host: '15.164.240.38',
-  port: 80  
+  // host: '15.164.240.38',
+  // port: 80  
+  host: 'google.com',
+  port: 80
 };
 
 // 1. 소켓 생성
@@ -12,7 +14,7 @@ var socket = new net.Socket();
 socket.connect(target.port, target.host, function(){
   console.log(`서버 접속. ${target.host}:${target.port}`);
   // 3. 서버와 메세지 송수신
-  socket.write('hello');
+  // socket.write('hello');
   socket.on('data', function(data){
     console.log('서버로부터 수신한 메세지: ' + data);
   });
