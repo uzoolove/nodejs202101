73fb2330cb66bdabfed54ccfbd9900fc9bc351fe
uzoolove <uzoolove@gmail.com>
2021-01-21 11:27:12 +0900

    .

--- server/chat/step6/bin/www.js
+++ server/chat/step6/bin/www.js
@@ -2,8 +2,12 @@ const http = require('http');
 const app = require('../app');
 
 // 1. http.Server 생성
-var tcpServer = http.createServer(app);
+var server = http.createServer(app);
 // 2. 포트 오픈 서버 구동
-tcpServer.listen(80, function(){
+server.listen(80, function(){
   console.log('HTTP 서버 구동.');
 });
+
+// socket.io 서버 구동
+var io = require('socket.io')(server);
+require('../chatserver')(io);
