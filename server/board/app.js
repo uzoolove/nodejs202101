7cf6f7a9bee30fc91705d4fc9d1a48dd7e87016e
uzoolove <uzoolove@gmail.com>
2021-01-21 13:44:09 +0900

    .

--- server/board/app.js
+++ server/board/app.js
@@ -24,11 +24,12 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, req.url + ' Not Found!!!'));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.error(err.stack);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
