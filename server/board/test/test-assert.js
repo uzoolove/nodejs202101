525d2a93bb81342f73c4aa6b095e435c963762a9
uzoolove <uzoolove@gmail.com>
2021-01-22 11:32:36 +0900

    .

--- server/board/test/test-assert.js
+++ server/board/test/test-assert.js
@@ -1,10 +1,12 @@
 var assert = require('assert');
+const { O_TRUNC } = require('constants');
 var upper = require('./upper');
 
 var success = 0;
 var fail = 0;
 console.log('테스트 시작.');
 
+/*
 var result = upper.sync('hello', 2);
 if(result == 'L'){
   console.log('1. 통과');
@@ -14,6 +16,81 @@ if(result == 'L'){
   fail++;
 }
 
-console.log('테스트 종료.');
-console.log('success', success);
-console.log('fail', fail);
+var result = upper.sync('hello', 5);
+if(result == null){
+  console.log('2. 통과');
+  success++;
+}else{
+  console.log('2. 실패');
+  fail++;
+}
+*/
+
+/* 독립적인 테스트를 위해서 작성해야 하는 코드가 너무 많다.
+try{
+  var result = upper.sync('hello', 5);
+  assert.strictEqual(result, null);
+  console.log('4. 통과.');
+  success++;
+}catch(err){
+  console.log('4. 실패.');
+  fail++;
+}
+
+try{
+  var result = upper.sync('hello', 2);
+  assert.strictEqual(result, 'L');
+  console.log('3. 통과.');
+  success++;
+}catch(err){
+  console.log('3. 실패.');
+  fail++;
+}
+*/
+
+/* 비동기 함수 테스트(순차적인 테스트가 필요할 경우 코드가 복잡해진다.)
+upper.async('hello', 5, function(result){
+  try{
+    assert.strictEqual(result, null);
+    console.log('1. 통과.');
+    success++;
+  }catch(err){
+    console.log('1. 실패.');
+    fail++;
+  }
+
+  upper.async('hello', 2, function(result){
+    try{
+      assert.strictEqual(result, 'L');
+      console.log('2. 통과.');
+      success++;
+    }catch(err){
+      console.log('2. 실패.');
+      fail++;
+    }
+
+    upper.async('hello', 0, function(result){
+      try{
+        assert.strictEqual(result, 'H');
+        console.log('3. 통과.');
+        success++;
+      }catch(err){
+        console.log('3. 실패.');
+        fail++;
+      }
+
+      console.log('테스트 종료.');
+      console.log('success', success);
+      console.log('fail', fail);
+    });
+  });
+});
+*/
+
+
+// console.log('테스트 종료.');
+// console.log('success', success);
+// console.log('fail', fail);
+
+
+
--- server/board/test/test-mocha.js
+++ server/board/test/test-mocha.js
@@ -1,7 +1,44 @@
 var assert = require('assert');
 var upper = require('./upper');
-var model = require('../models/board');
+// var model = require('../models/board');
 
 var article = {writer: '김철수', title: 'mocha 테스트', content: '김철수 만세'};
 
 
+// test suite
+describe('# upper 함수 테스트', function(){
+  describe('# 동기 테스트', function(){
+    // unit test
+    it('sync(hello, 2)', function(){
+      assert(upper.sync('hello', 2) === 'L');
+    });
+    it('sync(hello, 5)', function(){
+      assert.strictEqual(upper.sync('hello', 5), null);
+    });
+    it('sync(hello, 0)', function(){
+      assert.strictEqual(upper.sync('hello', 0), 'H');
+    });
+  });
+  
+  describe('# 비동기 테스트', function(){
+    // unit test
+    it('async(hello, 2)', function(done){
+      upper.async('hello', 2, function(result){
+        assert(result === 'L');
+        done();
+      });
+    });
+    it('async(hello, 5)', function(done){
+      upper.async('hello', 5, function(result){
+        assert.strictEqual(result, null);
+        done();
+      });
+    });
+    it('async(hello, 0)', function(done){
+      upper.async('hello', 0, function(result){
+        assert.strictEqual(result, 'H');
+        done();
+      });
+    });
+  });
+});
